38d0cf1086c99c44cf73f47621b2ed562e265b2b
weo <weo@melrosebridges-Mac-mini.local>
2012-07-02 16:50:34 -0700

    session change

--- packages/session/session.js
+++ packages/session/session.js
@@ -1,6 +1,8 @@
-function _Session() {
-  this.keys = [];
-  this.data = {};
+// XXX could use some tests
+_Session = function() {
+  this.keys = {};
+  this.key_deps = {};
+  this.key_value_deps = {};
 }
 
 _.extend(_Session.prototype, {
@@ -28,30 +30,92 @@ _.extend(_Session.prototype, {
 
   set: function (key, value) {
     var self = this;
+
+    var old_value = self.keys[key];
+    if (value === old_value)
+      return;
+    self.keys[key] = value;
+
+    var invalidate = function (map) {
+      if (map)
+        for (var id in map)
+          map[id].invalidate();
+    };
+
     self._ensureKey(key);
-    self.data[key].set(value);
+    invalidate(self.key_deps[key]);
+    invalidate(self.key_value_deps[key][old_value]);
+    invalidate(self.key_value_deps[key][value]);
   },
 
   get: function (key) {
     var self = this;
+    var context = Meteor.deps.Context.current;
     self._ensureKey(key);
-    return self.data[key]();
+
+    if (context && !(context.id in self.key_deps[key])) {
+      self.key_deps[key][context.id] = context;
+      context.on_invalidate(function () {
+        delete self.key_deps[key][context.id];
+      });
+    }
+
+    return self.keys[key];
+  },
+
+  all: function() {
+    var self = this;
+
+    var obj = {};
+    _.each(self.keys,function(value,key) {
+      obj[key] = self.get(key);
+    });
+    console.log('obj',obj)
+    return obj;
   },
 
   equals: function (key, value) {
     var self = this;
-    self._ensureKey(key);
-    return self.data[key].equals(value);
+    var context = Meteor.deps.Context.current;
+
+    if (typeof value !== 'string' &&
+        typeof value !== 'number' &&
+        typeof value !== 'boolean' &&
+        value !== null && value !== undefined)
+      throw new Error("Session.equals: value can't be an object");
+
+    if (context) {
+      self._ensureKey(key);
+      if (!(value in self.key_value_deps[key]))
+        self.key_value_deps[key][value] = {};
+
+      if (!(context.id in self.key_value_deps[key][value])) {
+        self.key_value_deps[key][value][context.id] = context;
+        context.on_invalidate(function () {
+          delete self.key_value_deps[key][value][context.id];
+
+          // clean up [key][value] if it's now empty, so we don't use
+          // O(n) memory for n = values seen ever
+          for (var x in self.key_value_deps[key][value])
+            return;
+          delete self.key_value_deps[key][value];
+        });
+      }
+    }
+
+    return self.keys[key] === value;
   },
 
   _ensureKey: function (key) {
     var self = this;
-    if (_.indexOf(self.keys, key) == -1) {
-      self.keys.push(key);
-      Meteor.deps.add_reactive_variable(self.data, key);
+    if (!(key in self.key_deps)) {
+      self.key_deps[key] = {};
+      self.key_value_deps[key] = {};
     }
   }
 });
+
+
 
 Session = new _Session;
 
@@ -59,19 +123,13 @@ Session = new _Session;
 if (Meteor._reload) {
   Meteor._reload.on_migrate('session', function () {
     // XXX sanitize and make sure it's JSONible?
-    var data = {};
-    _.each(Session.keys, function(key) {
-      data[key] = Session.data[key](true);
-    });
-    return [true, {data: data}];
+    return [true, {keys: Session.keys}];
   });
 
   (function () {
     var migration_data = Meteor._reload.migration_data('session');
-    if (migration_data && migration_data.data) {
-      for (var key in migration_data.data) {
-        Session.set(key, migration_data.data[key])
-      }
+    if (migration_data && migration_data.keys) {
+      Session.keys = migration_data.keys;
     }
   })();
 }
