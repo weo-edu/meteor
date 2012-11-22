997af77d7e6122b6d52e1aa609218f4d7f983b2c
Andrew Shaffer <andrew@weo.io>
2012-11-22 00:52:35 -0800

    added print

--- app/meteor/meteor.js
+++ app/meteor/meteor.js
@@ -767,6 +767,7 @@ Commands.push({
       else
         req.url = '/' + parts.slice(2).join('/');
 
+      console.log('upgrade received', app.name, req.url, parts, app.port);
       p.proxy.proxyWebSocketRequest(req, socket, head, 
         { host: '127.0.0.1', port: app.port });
     });
