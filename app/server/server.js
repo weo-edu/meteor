702d9b92a88fc3d73d0523bc9ecc1b478eb38a57
ashaffer <darawk@gmail.com>
2012-12-12 20:30:57 -0800

    Update app/server/server.js

--- app/server/server.js
+++ app/server/server.js
@@ -49,7 +49,7 @@ var init_keepalive = function () {
 
   setInterval(function () {
     keepalive_count ++;
-    if (keepalive_count >= 6) {
+    if (keepalive_count >= 16) {
       console.log("Failed to receive keepalive! Exiting.");
       process.exit(1);
     }
