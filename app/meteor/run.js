5c162446622d65ba49a6a7d89e038cccb1bb55d5
Andrew Shaffer <darawk@gmail.com>
2013-03-18 11:55:51 -0700

    added real parallelism to bundling process

--- app/meteor/run.js
+++ app/meteor/run.js
@@ -517,96 +517,100 @@ var kill_server = function (handle) {
     var ignore = [];
 
     console.log(env.METEOR_SUBAPP_NAME, 'start bundle', +new Date());
-    var oldcwd = process.cwd();
-    process.chdir(app_dir);
-    errors = bundler.bundle(app_dir, bundle_path, bundle_opts);
-    process.chdir(oldcwd);
-    console.log(env.METEOR_SUBAPP_NAME, 'end bundle', +new Date());
-
-    if (server_handle) {
-      //  If this is a restart, don't delete the
-      //  old bundle contents before rebundling
-      bundle_opts.dontRm = true;
-      kill_server(server_handle);
-    }
-    if (test_server_handle)
-      kill_server(test_server_handle);
-
-    var deps_raw;
-    try {
-      deps_raw =
-        fs.readFileSync(path.join(bundle_path, 'dependencies.json'), 'utf8');
-    } catch (e) {
-      if (!warned_about_no_deps_info) {
-        process.stdout.write("No dependency info in bundle. " +
-                             "Filesystem monitoring disabled.\n");
-        warned_about_no_deps_info = true;
+    var child = spawn('meteor', ['bundle'], {cwd: app_dir});
+    child.on('close', function(code) {
+      if(code !== 0) {
+        console.log('EXITING bundling error');
+        return;
       }
-    }
 
-    if (deps_raw)
-      deps_info = JSON.parse(deps_raw.toString());
+      console.log(env.METEOR_SUBAPP_NAME, 'end bundle', +new Date());
 
-    if (errors && errors.length != 0) {
-      log_to_clients({stdout: "Errors prevented startup:\n"}, env.METEOR_SUBAPP_NAME);
-      _.each(errors, function (e) {
-        log_to_clients({stdout: e + "\n"}, env.METEOR_SUBAPP_NAME);
-      });
-
-      if (!deps_info) {
-        // We don't know what files to watch for changes, so we have to exit.
-        process.stdout.write("\nPlease fix the problem and restart.\n");
-
-        // XXX calling process.exit like this leaves mongod running!
-        // One solution would be to try to kill mongo in this case. Or
-        // we could try to bundle before we launch mongo, so in this case
-        // mongo would never have been started.
-        process.exit(1);
+      if (server_handle) {
+        //  If this is a restart, don't delete the
+        //  old bundle contents before rebundling
+        bundle_opts.dontRm = true;
+        kill_server(server_handle);
       }
-      start_watching();
-      Status.hard_crashed(env.METEOR_SUBAPP_NAME);
-      return;
-    }
+      if (test_server_handle)
+        kill_server(test_server_handle);
 
-    start_watching();
-    Status.running = true;
-    server_handle = start_server(
-      bundle_path, outer_port, inner_port, mongo_url,
-      function () {
-        // on server exit
-        Status.running = false;
-        Status.listening = false;
-        Status.soft_crashed(env.METEOR_SUBAPP_NAME);
-        if (!Status.crashing)
-          restart_server();
-      }, function () {
-        // on listen
-        Status.listening = true;
-        _.each(request_queue, function (f) { f(); });
-        request_queue.length = 0;
-      }, app_dir, env);
+      var deps_raw;
+      try {
+        deps_raw =
+          fs.readFileSync(path.join(bundle_path, 'dependencies.json'), 'utf8');
+      } catch (e) {
+        if (!warned_about_no_deps_info) {
+          process.stdout.write("No dependency info in bundle. " +
+                               "Filesystem monitoring disabled.\n");
+          warned_about_no_deps_info = true;
+        }
+      }
 
+      if (deps_raw)
+        deps_info = JSON.parse(deps_raw.toString());
 
-    // launch test bundle and server if needed.
-    if (test_bundle_opts) {
-      var errors =
-        bundler.bundle(app_dir, test_bundle_path, test_bundle_opts);
-      if (errors) {
-        log_to_clients({system: "Errors prevented test server from starting:"}, env.METEOR_SUBAPP_NAME);
+      if (errors && errors.length != 0) {
+        log_to_clients({stdout: "Errors prevented startup:\n"}, env.METEOR_SUBAPP_NAME);
         _.each(errors, function (e) {
-          log_to_clients({system: e}, env.METEOR_SUBAPP_NAME);
+          log_to_clients({stdout: e + "\n"}, env.METEOR_SUBAPP_NAME);
         });
-        files.rm_recursive(test_bundle_path);
-      } else {
-        test_server_handle = start_server(
-          test_bundle_path, test_port, test_mongo_url, function () {
-            // No restarting or crash loop prevention on the test server
-            // for now. We'll see how annoying it is.
-            log_to_clients({'system': "Test server crashed."}, env.METEOR_SUBAPP_NAME);
-          });
+
+        if (!deps_info) {
+          // We don't know what files to watch for changes, so we have to exit.
+          process.stdout.write("\nPlease fix the problem and restart.\n");
+
+          // XXX calling process.exit like this leaves mongod running!
+          // One solution would be to try to kill mongo in this case. Or
+          // we could try to bundle before we launch mongo, so in this case
+          // mongo would never have been started.
+          process.exit(1);
+        }
+        start_watching();
+        Status.hard_crashed(env.METEOR_SUBAPP_NAME);
+        return;
       }
-    };
-  };
+
+      start_watching();
+      Status.running = true;
+      server_handle = start_server(
+        bundle_path, outer_port, inner_port, mongo_url,
+        function () {
+          // on server exit
+          Status.running = false;
+          Status.listening = false;
+          Status.soft_crashed(env.METEOR_SUBAPP_NAME);
+          if (!Status.crashing)
+            restart_server();
+        }, function () {
+          // on listen
+          Status.listening = true;
+          _.each(request_queue, function (f) { f(); });
+          request_queue.length = 0;
+        }, app_dir, env);
+
+
+      // launch test bundle and server if needed.
+      if (test_bundle_opts) {
+        var errors =
+          bundler.bundle(app_dir, test_bundle_path, test_bundle_opts);
+        if (errors) {
+          log_to_clients({system: "Errors prevented test server from starting:"}, env.METEOR_SUBAPP_NAME);
+          _.each(errors, function (e) {
+            log_to_clients({system: e}, env.METEOR_SUBAPP_NAME);
+          });
+          files.rm_recursive(test_bundle_path);
+        } else {
+          test_server_handle = start_server(
+            test_bundle_path, test_port, test_mongo_url, function () {
+              // No restarting or crash loop prevention on the test server
+              // for now. We'll see how annoying it is.
+              log_to_clients({'system': "Test server crashed."}, env.METEOR_SUBAPP_NAME);
+            });
+        }
+      };
+    });
+  }
 
 
   var mongo_err_count = 0;
