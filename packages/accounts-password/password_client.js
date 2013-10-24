b9e61e021a612fbffe7c5f0f9f8ddca754725b7d
Andrew Shaffer <andrew@weo.io>
2013-10-23 22:35:58 -0700

    removed a couple logs

--- packages/accounts-password/password_client.js
+++ packages/accounts-password/password_client.js
@@ -8,7 +8,6 @@
     // strip old password, replacing with the verifier object
     delete options.password;
     options.srp = verifier;
-    console.log('callLoginMethod createUser');
     Accounts.callLoginMethod({
       methodName: 'createUser',
       methodArguments: [options],
